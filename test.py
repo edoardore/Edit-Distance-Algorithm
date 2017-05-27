18e4d1b584c5ffca9e46344ef3f0e7362a75f871
edoardore <edoardore@me.com>
2017-05-27 13:04:52 +0200

    1.2

--- test.py
+++ test.py
@@ -12,6 +12,15 @@ def max(a, b):
     return b[ind], max
 
 
+def min(a, b):
+    min = float("inf")
+    for i in range(0, len(a)):
+        if min >= a[i]:
+            min = a[i]
+            ind = i
+    return b[ind], min
+
+
 def testA():
     T = []
     s = []
@@ -35,32 +44,42 @@ def testA():
         end = timer()
         time = end - start
         T.append(time)
-        print"------------->", s[j], max(list1, list2)
+        print"A------------->", s[j], max(list1, list2)
+
 
 def testB():
     T = []
     s = []
+    s.append("c")
     s.append("ca")
     s.append("cas")
     s.append("casa")
     s.append("casal")
     s.append("casale")
-    edit = DistanceManager()
-    dizionario = pickle.load(open("dizionario.p", "rb"))
+    length = len(s)
+    dizionario = pickle.load(open("dictionary.p", "rb"))
     list1 = []
     list2 = []
-    length = len(s)
-    for j in range(0, length):
+    for i in range(1, length):
         start = timer()
-        for i in range(0, len(dizionario)):
-            a = edit.indici_ngrammi(s[j])
-            b = edit.indici_ngrammi(dizionario[i])
-            list1.append(edit.jaccard(a, b))
-            list2.append(dizionario[i])
+        for j in range(1, len(dizionario)):
+            a = s[i]
+            a = list(a)
+            b = dizionario[j]
+            b = list(b)
+            edit = DistanceManager()
+            edit.edit_distance(a, b)
+            list1.append(edit.matrix[len(a)][len(b)])
+            list2.append(dizionario[j])
         end = timer()
-        time = end - start
-        T.append(time)
-        print"------------->", s[j], max(list1, list2)
+        tempo = end - start
+        T.append(tempo)
+        print"B------------->", s[i], min(list1, list2)
+    print T
+
+
+
 
 
 testA()
+testB()
